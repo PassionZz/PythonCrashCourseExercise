71409afe9537d5b547f53d9652c71437d0b31b68
passionzz <mr.mlisn@icloud.com>
2022-08-02 15:16:30 +0800

    chapter 3 part 2

---
+++ Chapter3/3-11.py
@@ -0,0 +1,15 @@
+"""
+@creat_time 2022/8/2 15:09
+@creat_by PassionZz
+@file_name 3-11
+@description 有意引发错误
+"""
+cars = ['Benz','Porsche','Tesla','Volvo','Toyota']
+length = len(cars)
+# print(cars[length])
+print(cars[length-1])
+print(cars[-1])
+
+cars = []
+# print(cars[-1])
+print(cars)
---
+++ Chapter3/3-8.py
@@ -0,0 +1,29 @@
+"""
+@creat_time 2022/8/2 14:50
+@creat_by PassionZz
+@file_name 3-8
+@description 5个渴望去旅游的地方
+"""
+travel_place = ['Beijing','Iceland','Canada','London','Japan']
+print(travel_place)
+
+print(sorted(travel_place))
+
+print(travel_place)
+
+print(sorted(travel_place,reverse=True))
+
+print(travel_place)
+
+travel_place.reverse()
+print(travel_place)
+
+travel_place.reverse()
+print(travel_place)
+
+travel_place.sort()
+print(travel_place)
+
+travel_place.sort(reverse=True)
+print(travel_place)
+
